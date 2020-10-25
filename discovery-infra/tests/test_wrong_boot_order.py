261f1ce53e16876ef34e3cb0d40992cbb3eff412
Lital Alon <lalon@redhat.com>
2020-10-25 15:32:59 +0200

    added wrong boot order e2e flow

--- discovery-infra/tests/test_wrong_boot_order.py
+++ discovery-infra/tests/test_wrong_boot_order.py
@@ -2,18 +2,18 @@ import pytest
 
 from tests.base_test import BaseTest
 
-NODE = 'master-0-0'
-
 
 class TestWrongBootOrder(BaseTest):
     @pytest.mark.regression
-    def test_wrong_boot_order_one_master(self, api_client, node_controller):
+    def test_wrong_boot_order_one_node(self, api_client, node_controller, cluster):
         # Define new cluster
-        cluster_id = self.create_cluster(api_client=api_client).id
+        cluster_id = cluster().id
         # Generate and download cluster ISO
         self.generate_and_download_image(cluster_id=cluster_id, api_client=api_client)
-        # Change boot order of master-0-0 TODO
-        node_controller.set_boot_order_to_node(vm_name=NODE, cd_first=True)
+        # Change boot order of a random node
+        hosts = node_controller.list_nodes()
+        node = list(hosts.values())[0]
+        node.set_boot_order(cd_first=True)
         # Boot nodes into ISO
         node_controller.start_all_nodes()
         # Wait until hosts are discovered and update host roles
@@ -25,13 +25,17 @@ class TestWrongBootOrder(BaseTest):
                                 )
         # Start cluster install
         self.start_cluster_install(cluster_id=cluster_id, api_client=api_client)
-        # Cancel cluster install once cluster installation start
         self.wait_for_installing_in_progress(cluster_id=cluster_id, api_client=api_client)
-        # self.wait_for_wrong_boot_order(cluster_id=cluster_id, api_client=api_client)
+        # Wait until wrong boot order
+        self.wait_for_one_host_to_be_in_wrong_boot_order(cluster_id=cluster_id,
+                                                         api_client=api_client)
         # Reboot required nodes into ISO
-        # self.reboot_nodes_into_iso_after_wrong_boot_order()
+        node.shutdown()
+        node.set_boot_order(cd_first=False)
+        node.start()
         # Wait for host to keep installing
-        # self.wait_until_host_in_status()
+        self.wait_for_nodes_status_installing_or_installed(cluster_id=cluster_id,
+                                                           api_client=api_client)
         # wait until all nodes are in Installed status, will fail in case one host in error
-        # self.wait_for_nodes_to_install(cluster_id=cluster_id, api_client=api_client)
-        # self.wait_for_cluster_to_install(cluster_id=cluster_id, api_client=api_client)
+        self.wait_for_nodes_to_install(cluster_id=cluster_id, api_client=api_client)
+        self.wait_for_cluster_to_install(cluster_id=cluster_id, api_client=api_client)
